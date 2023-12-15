e59c69d647c848e0f27d23dddecdfd95349fd5aa
Emediong Bassey <basseyemediong@gmail.com>
2023-12-15 15:31:23 +0100

    nav update

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,14 +6,12 @@
 						<!-- Logo
 						============================================= -->
 						<div id="logo">
-							<a href="index.html" class="standard-logo"><img src="{{ url('assets/images/Aliveng.png') }}" alt="Alive Logo"></a>
-							<a href="index.html" class="retina-logo"><img src="demos/ecommerce/images/logo@2x.png" alt="Alive Logo"></a>
+							<a href="" class="standard-logo"><img src="{{ url('assets/images/Aliveng.png') }}" alt="Alive Logo"></a>
+							<a href="" class="retina-logo"><img src="{{ url('assets/images/Aliveng.png') }}" alt="Alive Logo"></a>
 						</div><!-- #logo end -->
 
 
-						<div id="primary-menu-trigger">
-							<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
-						</div>
+						
 
 						<!-- Primary Navigation
 						============================================= -->
@@ -21,10 +19,10 @@
 
 
 						</nav><!-- #primary-menu end -->
-
+{{-- 
 						<form class="top-search-form" action="search.html" method="get">
 							<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter.." autocomplete="off">
-						</form>
+						</form> --}}
 
 					</div>
 				</div>
